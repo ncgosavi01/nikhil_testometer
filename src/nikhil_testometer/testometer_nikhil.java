b4d50b6c290d70e6ad630913e027cdfe2ca821ca
Admin <Admin@LAPTOP-D08B1N66>
2023-10-30 10:08:29 +0530

    1st program

---
+++ src/nikhil_testometer/testometer_nikhil.java
@@ -0,0 +1,10 @@
+package nikhil_testometer;
+
+public class testometer_nikhil 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("HELLO");
+	}
+
+}
---
+++ src/nikhil_testometer/testometer_nov_23.java
@@ -0,0 +1,11 @@
+package nikhil_testometer;
+
+public class testometer_nov_23 
+{
+	public static void main(String[] args) 
+	{
+	System.out.println("Hello");
+	}
+}
+
+
