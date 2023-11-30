e9f3f8ec447c976f64780e25fb43c714951a7753
Admin <Admin@LAPTOP-D08B1N66>
2023-11-30 19:28:11 +0530

    update

--- src/selenium_testometer/ObjectandClass.java
+++ src/selenium_testometer/ObjectandClass.java
@@ -2,4 +2,206 @@ package selenium_testometer;
 
 public class ObjectandClass {
 
-}
+		public static void main (String[] args) {
+
+			class Spoon
+			{
+				String height;
+				String shape;
+				int size;
+
+				void eat() {
+
+				}
+			}
+
+			class refrigerator
+			{
+				int weight;
+				String compressor;
+				String company;
+				Long Energy_Efficiency;
+
+				void Cool() {
+
+				}
+
+			}
+
+			class pen
+			{
+
+				String type;
+				String color;
+				double Width;
+
+				void write() {
+
+				}
+			}
+
+			class table
+
+			{
+
+				double lenght;
+				String shape;
+				double width;
+				double height;
+
+				void organize() {
+
+				}
+
+
+
+			}
+
+
+			class toothbrush
+			{
+				double lenght;
+				double width;
+				double height;
+				String material;
+
+				void Cleaning_teeth() {
+
+				}
+
+
+			}
+
+			class books
+			{
+				int pages;
+				String content;
+				double weight;
+				String material;
+
+				void Reading() {
+				}
+
+
+			}
+
+			class notebook
+			{
+				int size;
+				int pages;
+				String type;
+				String shape;
+
+				void Write() {
+
+				}
+			}
+
+			class Mobile 
+			{
+
+				String name;
+				int price;
+				String OS; 
+				String features;
+
+				void calling() {
+
+				}
+
+			}
+
+			class clothes
+			{
+
+				String color;
+				String material;
+				String strenght;
+
+				void wear() {
+				}
+			}
+
+			class Bag
+			{
+
+				String brand;
+				double size;
+				double weight;
+
+				void carry_things() {
+				}
+			}
+
+			class knife
+			{
+				String material;
+				double weight;
+				double height;
+
+				void cutting() {
+
+				}
+			}  
+
+			class car
+			{
+				String color;
+				String model;
+				int price;
+
+				void drive(){
+
+				} 
+			}
+
+			class Bed
+			{
+				int weight;
+				double Height;
+				double Width;
+
+				void sleep() {
+
+				}
+			}
+
+			class Laptop
+			{
+			 String brand;
+			 String color;
+			 double weight;
+			 int RAM;
+			 String Model;
+			 
+			 void work() {
+				 
+			 }
+			}
+			
+			class SportMan
+			{
+				
+				String name;
+				String Team;
+				String sportsName;
+				 
+				void play() {
+					
+				}
+			}
+			
+			class TestCase
+			{
+				String  name;
+				String description;
+				String status;
+				
+				void execute() {
+					
+				}
+			}
+			
+			
+		}
+	}
